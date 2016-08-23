e220979df618b9809ad0ad0806ed444e3c8a3da3
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-08-23 14:37:50 +0200

    testing: made matchings case insensitive

--- MasterThesis/MasterThesis/simple_label_matcher.py
+++ MasterThesis/MasterThesis/simple_label_matcher.py
@@ -31,17 +31,17 @@ def match_two_ontologies(onto, onto1):
                 if i.get_child("{http://www.w3.org/2000/01/rdf-schema#}comment") != None:
                     util.write2File("regex.txt", "-> " + i.get_child("{http://www.w3.org/2000/01/rdf-schema#}comment").get_text() + "\n", "a")
                 for j in onto1_elements:
-                    match_result = re.match(".*" + i.name + ".*", j.name)
+                    match_result = re.match(".*" + i.name + ".*", j.name, re.IGNORECASE)
                     if match_result:
                         util.write2File("matching.txt", "Nodes " + i.name + " and " + j.name + " are similar, because of the name\n", "a")
                     label = i.get_child("{http://www.w3.org/2000/01/rdf-schema#}comment")
                     if label != None:
-                        match_result = re.match(".*" + label.get_text() + ".*", j.name)
+                        match_result = re.match(".*" + label.get_text() + ".*", j.name, re.IGNORECASE)
                         if match_result:
                             util.write2File("matching.txt", "Nodes " + i.name + " and " + j.name + " are similar, because of the label: " + label.get_text() + "\n", "a")
                     comment = i.get_child("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}comment")
                     if comment != None:
-                        match_result = re.match(".*" + comment.get_text() + ".*", j.name)
+                        match_result = re.match(".*" + comment.get_text() + ".*", j.name, re.IGNORECASE)
                         if match_result:
                             util.write2File("matching.txt", "Nodes " + i.name + " and " + j.name + " are similar, because of the comment\n", "a")
             except re.error:
@@ -55,17 +55,17 @@ def match_two_ontologies(onto, onto1):
                 if k.get_child("{http://www.w3.org/2000/01/rdf-schema#}comment") != None:
                     util.write2File("regex.txt", "-> " + k.get_child("{http://www.w3.org/2000/01/rdf-schema#}comment").get_text() + "\n", "a")
                 for l in onto_elements:
-                    match_result1 = re.match(".*" + k.name + ".*", l.name)
+                    match_result1 = re.match(".*" + k.name + ".*", l.name, re.IGNORECASE)
                     if match_result1:
                         util.write2File("matching.txt", "Nodes " + k.name + " and " + l.name + " are similar, because of the name\n", "a")
                     label1 = k.get_child("{http://www.w3.org/2000/01/rdf-schema#}label")
                     if label1 != None:
-                        match_result1 = re.match(".*" + label1.get_text() + ".*", l.name)
+                        match_result1 = re.match(".*" + label1.get_text() + ".*", l.name, re.IGNORECASE)
                         if match_result1:
                             util.write2File("matching.txt", "Nodes " + k.name + " and " + l.name + " are similar, because of the label: " + label1.get_text() + "\n", "a")
                     comment1 = k.get_child("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}comment")
                     if comment1 != None:
-                        match_result1 = re.match(".*" + comment1.get_text() + ".*", l.name)
+                        match_result1 = re.match(".*" + comment1.get_text() + ".*", l.name, re.IGNORECASE)
                         if match_result1:
                             util.write2File("matching.txt", "Nodes " + k.name + " and " + l.name + " are similar, because of the comment\n", "a")
             except re.error:
