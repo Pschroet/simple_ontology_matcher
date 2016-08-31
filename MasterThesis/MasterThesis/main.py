f9bf96a12ba7e584f4365874a573c99895a837f1
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-08-31 11:50:32 +0200

    both cases (HS and HCC) are now present in test setup

--- MasterThesis/MasterThesis/main.py
+++ MasterThesis/MasterThesis/main.py
@@ -10,6 +10,7 @@ import matcher
 if __name__ == '__main__':
     r = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/AnimalMotionOntology.owl")
     r1 = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/envo.owl")
+    matcher.ontology_matcher("hs_matcher", [r.ontology, r1.ontology])
     #r = reader.ontology_reader("owl_rdfxml_parser", "cafe.owl")
     #r1 = reader.ontology_reader("owl_rdfxml_parser", "envo.owl")
-    matcher.ontology_matcher("simple_label_matcher", [r.ontology, r1.ontology])
+    #matcher.ontology_matcher("simple_label_matcher", [r.ontology, r1.ontology])
