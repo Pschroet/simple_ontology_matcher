242214130ea2d71db6a46a1a8ca0b9c8a3782cc5
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-08-31 13:43:01 +0200

    added tool chain and configuration files

--- MasterThesis/MasterThesis/main.py
+++ MasterThesis/MasterThesis/main.py
@@ -11,7 +11,8 @@ import reader
 if __name__ == '__main__':
     r = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/AnimalMotionOntology.owl")
     r1 = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/envo.owl")
-    chain = matching_tool_chain.tool_chain("./hs-config.xml")
+    #chain = matching_tool_chain.tool_chain("./hs-config.xml")
+    chain = matching_tool_chain.tool_chain("./test-config.xml")
     chain.match_ontologies([r.ontology, r1.ontology])
     #r = reader.ontology_reader("owl_rdfxml_parser", "cafe.owl")
     #r1 = reader.ontology_reader("owl_rdfxml_parser", "envo.owl")
---
+++ MasterThesis/MasterThesis/matching_tool_chain.py
@@ -0,0 +1,37 @@
+'''
+Created on 31.08.2016
+
+@author: philipp
+'''
+
+import defusedxml.ElementTree
+import matcher
+
+class tool_chain(object):
+    '''
+    classdocs
+    '''
+
+    config = ""
+    matching_tools = []
+    ontologies = []
+
+    def __init__(self, config):
+        '''
+        Constructor
+        '''
+        self.config = config
+        tree = defusedxml.ElementTree.parse(self.config)
+        root = tree.getroot()
+        for item in root.getchildren():
+            if item.tag == "matcher":
+                self.matching_tools.append(item.text)
+
+    #read an ontology
+    def match_ontologies(self, ontologies = []):
+        if len(ontologies) > 2:
+            pass
+        elif len(ontologies) == 2:
+            for matching_tool in self.matching_tools:
+                tmp_tool = matcher.ontology_matcher(matching_tool, ontologies)
+                tmp_tool.match_two_ontologies()
