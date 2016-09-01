23205e3ba86c8d9f123131b29bef08c461ed25f0
Pschroet <pschroet@inf.fu-berlin.de>
2016-09-01 20:27:52 +0200

    added Levenshtein distance calculation to matcher

--- MasterThesis/MasterThesis/matcher/simple_label_matcher.py
+++ MasterThesis/MasterThesis/matcher/simple_label_matcher.py
@@ -4,6 +4,8 @@ Created on 19.08.2016
 @author: Philipp Schroeter
 '''
 
+import imp
+import logging
 import ontology
 import os
 import re
@@ -13,6 +15,7 @@ import util
 #tries to match two ontologies just by comapring the labels of nodes
 #returns a bridge ontology, which holds information about nodes that are assumed to be linked on some way
 def match_two_ontologies(onto, onto1):
+    #ensure that there are actually ontologies to compare
     if onto is not None and onto1 is not None:
         #the current ontology, which is compared to the other ones
         bridge_ontology = ontology.ontology("bridge-" + str(time.localtime()))
@@ -39,3 +42,15 @@ def match_two_ontologies(onto, onto1):
             except re.error:
                 #just ignore errors during regular expression operations and try to go on
                 pass
+        #go through all elements and calculate the Levenshtein distance
+        dist_calc = util.distance_calculator()
+        for i in onto_elements:
+            label = i.get_child("{http://www.w3.org/2000/01/rdf-schema#}label")
+            if label != None:
+                for j in onto1_elements:
+                    label1 = j.get_child("{http://www.w3.org/2000/01/rdf-schema#}label")
+                    if label1 != None:
+                        distance = dist_calc.calculate_distance(label.get_text(), label1.get_text())
+                        #if the labels are not the same, but are similar, the nodes might be, too
+                        if distance > 0 and distance < 5:
+                            util.write2File("distances.txt", "Nodes " + i.name + "(" + label.get_text() + ")" + " and " + j.name + "(" + label1.get_text() + ")" + " have the Levenshtein distance: " + str(distance) + "\n", "a")
