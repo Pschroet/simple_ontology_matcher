2f120816e5940004091e8380d6f0044d49fd858c
Pschroet <pschroet@inf.fu-berlin.de>
2016-09-01 20:24:57 +0200

    fixed config

--- MasterThesis/MasterThesis/main.py
+++ MasterThesis/MasterThesis/main.py
@@ -9,12 +9,12 @@ import reader
 #import matcher
 
 if __name__ == '__main__':
-    r = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/AnimalMotionOntology.owl")
-    r1 = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/envo.owl")
+    #r = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/AnimalMotionOntology.owl")
+    #r1 = reader.ontology_reader("owl_rdfxml_parser", "/home/philipp/Dokumente/Ontologie/envo.owl")
     #chain = matching_tool_chain.tool_chain("./hs-config.xml")
-    chain = matching_tool_chain.tool_chain("./test-config.xml")
-    chain.match_ontologies([r.ontology, r1.ontology])
-    #r = reader.ontology_reader("owl_rdfxml_parser", "cafe.owl")
-    #r1 = reader.ontology_reader("owl_rdfxml_parser", "envo.owl")
     #chain = matching_tool_chain.tool_chain("./test-config.xml")
     #chain.match_ontologies([r.ontology, r1.ontology])
+    r = reader.ontology_reader("owl_rdfxml_parser", "cafe.owl")
+    r1 = reader.ontology_reader("owl_rdfxml_parser", "envo.owl")
+    chain = matching_tool_chain.tool_chain("./test-config.xml")
+    chain.match_ontologies([r.ontology, r1.ontology])
