36b7f55bf17e08d5c5f7dc1bd276697ee92f8c83
Pschroet <pschroet@inf.fu-berlin.de>
2016-09-01 23:01:00 +0200

    improved matching with Levenshtein distance

--- MasterThesis/MasterThesis/matcher/simple_label_matcher.py
+++ MasterThesis/MasterThesis/matcher/simple_label_matcher.py
@@ -15,6 +15,7 @@ import util
 #tries to match two ontologies just by comapring the labels of nodes
 #returns a bridge ontology, which holds information about nodes that are assumed to be linked on some way
 def match_two_ontologies(onto, onto1):
+    dist_calc = util.distance_calculator()
     #ensure that there are actually ontologies to compare
     if onto is not None and onto1 is not None:
         #the current ontology, which is compared to the other ones
@@ -34,6 +35,12 @@ def match_two_ontologies(onto, onto1):
                             match_result = re.match(label.get_text(), label1.get_text(), re.IGNORECASE)
                             if match_result:
                                 util.write2File("matching.txt", "Nodes " + i.name + " and " + j.name + " have the same label: " + label.get_text() + "\n", "a")
+                        #in addition calculate the Levenshtein distance
+                        elif label1 != None and len(label.get_text()) > 3 and len(label1.get_text()) > 3:
+                            distance = dist_calc.calculate_distance(label.get_text(), label1.get_text())
+                            #if the labels are not the same, but are similar, the nodes might be, too
+                            if distance > 0 and distance < 2 and ((distance/2) <= len(label.get_text()) or (distance/2) <= len(label1.get_text())):
+                                util.write2File("matching.txt", "Nodes " + i.name + "(" + label.get_text() + ")" + " and " + j.name + "(" + label1.get_text() + ")" + " have the Levenshtein distance: " + str(distance) + "\n", "a")
                     #comment = i.get_child("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}comment")
                     #if comment != None:
                     #    match_result = re.match(".*" + comment.get_text() + ".*", j.name, re.IGNORECASE)
@@ -42,15 +49,3 @@ def match_two_ontologies(onto, onto1):
             except re.error:
                 #just ignore errors during regular expression operations and try to go on
                 pass
-        #go through all elements and calculate the Levenshtein distance
-        dist_calc = util.distance_calculator()
-        for i in onto_elements:
-            label = i.get_child("{http://www.w3.org/2000/01/rdf-schema#}label")
-            if label != None:
-                for j in onto1_elements:
-                    label1 = j.get_child("{http://www.w3.org/2000/01/rdf-schema#}label")
-                    if label1 != None:
-                        distance = dist_calc.calculate_distance(label.get_text(), label1.get_text())
-                        #if the labels are not the same, but are similar, the nodes might be, too
-                        if distance > 0 and distance < 5:
-                            util.write2File("distances.txt", "Nodes " + i.name + "(" + label.get_text() + ")" + " and " + j.name + "(" + label1.get_text() + ")" + " have the Levenshtein distance: " + str(distance) + "\n", "a")
