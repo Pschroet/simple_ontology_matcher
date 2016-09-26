23af88472334c2ce12da42ec3b283b5895eaee89
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-09-26 17:27:48 +0200

    modularized use of dictionary, so different dictionaries can be imported and used, if they exist on the system

--- MasterThesis/OntologyMatcher/matcher/synonym_matcher.py
+++ MasterThesis/OntologyMatcher/matcher/synonym_matcher.py
@@ -4,14 +4,14 @@ Created on 13.09.2016
 @author: Philipp Schroeter
 '''
 
-import PyDictionary
 import re
+import util
 
 #tries to match two ontologies just by comapring the labels of nodes
 #returns a list, which holds information about nodes that are assumed to be linked on some way
 # each item in the list has the following elements (in this order): IRI of the first element, it's label, the IRI of the second element, it's label, a string that states, why those elements have been chosen
 def match_two_ontologies(results, onto, onto1):
-    dictionary = PyDictionary.PyDictionary()
+    dictionary = util.dictionary_wrapper()
     #ensure that there are actually ontologies to compare
     if onto is not None and onto1 is not None:
         connections = {"matches":[], "text": "Synonyms"}
