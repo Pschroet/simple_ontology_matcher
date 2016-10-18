92bdc2277b83c3356bb9c662bb995dd6828c68f8
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-10-18 16:54:32 +0200

    fixed template not working when not being used as part of Django

--- MasterThesis/OntologyMatcher/main.py
+++ MasterThesis/OntologyMatcher/main.py
@@ -8,8 +8,10 @@ import matching_tool_chain
 import reader
 import util
 import os
-#import PyDictionary
+import PyDictionary
+from django.conf import settings
 from django.template import Context, Template
+from django.template.engine import Engine
 
 if __name__ == '__main__':
     #testing readers and matchers in general
@@ -21,16 +23,18 @@ if __name__ == '__main__':
     #chain.match_ontologies([r.ontology, r1.ontology])
     ##############################
     #testing local HTML template parsing
-    r = reader.ontology_reader("owl_rdfxml_parser", "ontologies/cafe-test.owl")
-    r1 = reader.ontology_reader("owl_rdfxml_parser", "ontologies/envo-test.owl")
-    chain = matching_tool_chain.tool_chain()
-    chain.add_config_from_file("./all_matchers-config.xml")
-    result = chain.match_ontologies([r.ontology, r1.ontology])
-    context = {"title":"Matched Ontologies", "results":result}
-    template_raw = util.readFileContentAsString(os.path.dirname(__file__) + "/result_writer/matching_result.html")
-    template_content = Template(template_raw)
-    util.write2File("matches.html", template_content.render(Context(context)), "w")
+    #r = reader.ontology_reader("owl_rdfxml_parser", "ontologies/cafe-test.owl")
+    #r1 = reader.ontology_reader("owl_rdfxml_parser", "ontologies/envo-test.owl")
+    #chain = matching_tool_chain.tool_chain()
+    #chain.add_config_from_file("./all_matchers-config.xml")
+    #result = chain.match_ontologies([r.ontology, r1.ontology])
+    #settings.configure(DEBUG=False)
+    #context = {"title":"Matched Ontologies", "results":result}
+    #template_raw = util.readFileContentAsString(os.path.dirname(__file__) + "/result_writer/matching_result.html")
+    #template_content = Template(template_raw, engine=Engine())
+    #util.write2File("matches.html", template_content.render(Context(context)), "w")
     ##############################
     #for testing PyDictionary
-    #dictionary = PyDictionary.PyDictionary()
-    #print dictionary.synonym("m3u")
+    dictionary = PyDictionary.PyDictionary()
+    print dictionary.synonym("test")
+    print dictionary.synonym("search")
