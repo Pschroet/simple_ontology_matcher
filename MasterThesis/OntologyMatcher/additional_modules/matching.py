f16774ca6ad1850d90c399f0d123909c96b3c36f
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-10-26 14:22:36 +0200

    implemented a matching data type (and defined what it is) and fixed reader for matching files

--- MasterThesis/OntologyMatcher/additional_modules/matching.py
+++ MasterThesis/OntologyMatcher/additional_modules/matching.py
@@ -1,27 +1,40 @@
 '''
 Created on 25.10.2016
 
-@author: Philipp Schröter
+@author: Philipp Schroeter
 '''
 
-class matchings(object):
+class matching(object):
     '''
-    A class to save a matching of two or more ontologies
+    A class to save a matching of two or more elements, contains
+    - the elements themself of the matching (just a list of elements, usually strings)
+    - the properties of the matching (expected is a dictionary)
     '''
     elements = []
-    properties = []
+    properties = {}
     
     def __init__(self):
         pass
-        
+    
+    #add a single element
     def add_element(self, element):
         self.elements.append(element)
     
+    #add multiple elements as a list
     def add_elements(self, elements):
         self.elements = self.elements + elements
-        
-    def add_property(self, prop):
-        self.properties.append(prop)
     
+    #add a property as a key value pair
+    def add_property(self, key, value):
+        self.properties[key] = value
+    
+    #add an amount of properties from a dictionary
     def add_properties(self, properties):
-        self.properties = self.properties + properties
+        for prop in properties:
+            self.properties[prop] = properties[prop]
+    
+    def tostring(self):
+        output = "Elements: " + str(self.elements)
+        for prop in self.properties:
+            output = output + ", Property: " + prop + ": " + self.properties[prop]
+        return output
--- MasterThesis/OntologyMatcher/additional_modules/xmlrdf_reader.py
+++ MasterThesis/OntologyMatcher/additional_modules/xmlrdf_reader.py
@@ -8,18 +8,26 @@ import defusedxml.ElementTree
 import matching
 import re
 
-def parse_ontology_file(ontology_file):
-    print "Parsing " + ontology_file
-    tree = defusedxml.ElementTree.parse(ontology_file)
-    root = tree.getroot()
+def parse_matchings_file(matchings_file):
+    print "Parsing matching file " + matchings_file
+    tree = defusedxml.ElementTree.parse(matchings_file)
+    root = tree.getroot().find("{http://knowledgeweb.semanticweb.org/heterogeneity/alignment}Alignment")
     #check if the file could even be an OWL file in RDF XML syntax
-    if root.tag == "Alignment":
-        matchings = matching.matchings()
-        children = root.findall("Cell").getchildren()
-        for child in children:
-            if re.match("entity[0-9]*", child.tag, re.IGNORECASE):
-                matchings.add_element(child)
-            elif re.match("measure", child.tag, re.IGNORECASE):
-                matchings.add_property(child)
-            elif re.match("relation", child.tag, re.IGNORECASE):
-                matchings.add_property(child)
+    if root.tag == "{http://knowledgeweb.semanticweb.org/heterogeneity/alignment}Alignment":
+        matchings = []
+        maps = root.findall("{http://knowledgeweb.semanticweb.org/heterogeneity/alignment}map")
+        #print maps
+        for map in maps:
+            match = matching.matching()
+            children = map.find("{http://knowledgeweb.semanticweb.org/heterogeneity/alignment}Cell").getchildren()
+            #print children
+            for child in children:
+                #print child
+                if re.match("[{]http[:][/][/]knowledgeweb[.]semanticweb[.]org[/]heterogeneity[/]alignment[}]entity[0-9]*", child.tag, re.IGNORECASE):
+                    match.add_element(child.attrib["{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource"])
+                elif re.match("[{]http[:][/][/]knowledgeweb[.]semanticweb[.]org[/]heterogeneity[/]alignment[}]measure", child.tag, re.IGNORECASE):
+                    match.add_property(child.tag, child.text)
+                elif re.match("[{]http[:][/][/]knowledgeweb[.]semanticweb[.]org[/]heterogeneity[/]alignment[}]relation", child.tag, re.IGNORECASE):
+                    match.add_property(child.tag, child.text)
+            matchings.append(match)
+        return matchings
