8ecb6d1c0eb38ddd91238e8674a4cccaf3854c84
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2016-10-26 14:45:57 +0200

    added module for comparing matchings

---
+++ MasterThesis/OntologyMatcher/additional_modules/compare_matchings.py
@@ -0,0 +1,9 @@
+'''
+Created on 26.10.2016
+
+@author: Philipp Schroeter
+'''
+
+#compares two matchings and returns a list for all matchings that are in both of them
+def compare(matchings):
+    pass
