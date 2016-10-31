6cb212a264f94696973cf8a6bf881e5df5340292
Pschroet <pschroet@inf.fu-berlin.de>
2016-10-31 22:18:39 +0100

    implemented usage of matching comparison

--- MasterThesis/OntologyMatcher/main.py
+++ MasterThesis/OntologyMatcher/main.py
@@ -8,10 +8,12 @@ import matching_tool_chain
 import reader
 import util
 import os
-import PyDictionary
 from django.conf import settings
 from django.template import Context, Template
 from django.template.engine import Engine
+import PyDictionary
+import xmlrdf_reader
+import compare_matchings
 
 if __name__ == '__main__':
     #testing readers and matchers in general
@@ -35,6 +37,16 @@ if __name__ == '__main__':
     #util.write2File("matches.html", template_content.render(Context(context)), "w")
     ##############################
     #for testing PyDictionary
-    dictionary = PyDictionary.PyDictionary()
-    print dictionary.synonym("test")
-    print dictionary.synonym("search")
+    #dictionary = PyDictionary.PyDictionary()
+    #print dictionary.synonym("test")
+    #print dictionary.synonym("search")
+    ##############################
+    #for checking the results of the matchers
+    matching = xmlrdf_reader.parse_matchings_file("./additional_modules/matches-1.rdf")
+    matching1 = xmlrdf_reader.parse_matchings_file("./additional_modules/envo_pato.rdf")
+    result = compare_matchings.compare(matching, matching1)
+    settings.configure(DEBUG=False)
+    context = {"title":"Matching Comparison", "same":result[0], "different":result[1]}
+    template_raw = util.readFileContentAsString(os.path.dirname(__file__) + "./additional_modules/matching_comparison.html")
+    template_content = Template(template_raw, engine=Engine())
+    util.write2File("matching_comparison.html", template_content.render(Context(context)), "w")
