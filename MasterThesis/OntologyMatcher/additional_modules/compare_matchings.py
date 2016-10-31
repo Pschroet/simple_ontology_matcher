cbbb1d1a820585eecfe122220d01a72a46081545
Pschroet <pschroet@inf.fu-berlin.de>
2016-10-31 22:17:00 +0100

    implemented comparison of matchings

--- MasterThesis/OntologyMatcher/additional_modules/compare_matchings.py
+++ MasterThesis/OntologyMatcher/additional_modules/compare_matchings.py
@@ -5,5 +5,16 @@ Created on 26.10.2016
 '''
 
 #compares two matchings and returns a list for all matchings that are in both of them
-def compare(matchings):
-    pass
+# returns two lists, the first with all matchings that are equal, the second for all which are different
+def compare(matching1, matching2):
+    same = []
+    different = []
+    for matching in matching1:
+        for comparison in matching2:
+            if comparison.is_same(matching.elements):
+                same.append(matching.elements)
+            else:
+                different.append(matching.elements)
+    #print "same: " + str(same)
+    #print "different: " + str(different)
+    return [same, different]
