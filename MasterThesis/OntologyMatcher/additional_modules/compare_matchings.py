297925c7ef06a4fbf11c19c2837d9981468bac60
Pschroet <pschroet@inf.fu-berlin.de>
2016-11-10 00:12:33 +0100

    improved comparison by separating differences by lists of matchings, ignoring that are in these lists, because they are the same element

--- MasterThesis/OntologyMatcher/additional_modules/compare_matchings.py
+++ MasterThesis/OntologyMatcher/additional_modules/compare_matchings.py
@@ -4,15 +4,19 @@ Created on 26.10.2016
 @author: Philipp Schroeter
 '''
 
+import re
+
 #compares two matchings and returns a list for all matchings that are in both of them
 # returns two lists, the first with all matchings that are equal, the second for all which are different
 def compare(matching1, matching2):
     same = []
-    different = []
+    different = [[], []]
     for matching in matching1:
+        matching_found = False
         for comparison in matching2:
             #print str(matching.elements) + " == " + str(comparison.elements)
             tmp = ""
+            #if matching.is_same(comparison.elements) and comparison.elements[0] != comparison.elements[1]:
             if matching.is_same(comparison.elements):
                 tmp = list(matching.elements)
                 labels = matching.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
@@ -25,18 +29,39 @@ def compare(matching1, matching2):
                 #print labels
                 #print tmp
                 same.append(tmp)
+                matching_found = True
+        #if not matching_found and matching.elements[0] != matching.elements[1]:
+        if not matching_found:
+            tmp = list(matching.elements)
+            labels = matching.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
+            if labels is not None:
+                tmp = tmp + labels
             else:
-                tmp = list(matching.elements)
-                labels = matching.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
+                labels = comparison.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
                 if labels is not None:
                     tmp = tmp + labels
-                else:
-                    labels = comparison.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
-                    if labels is not None:
-                        tmp = tmp + labels
-                #print labels
-                #print tmp
-                different.append(tmp)
-    #print "same: " + str(same)
-    #print "different: " + str(different)
+            #print labels
+            #print tmp
+            different[0].append(tmp)
+    #check the second ontology/terminology for items not being the same
+    for matching in matching2:
+        matching_found = False
+        for item in same:
+            if (matching.elements[0] == item[0] and matching.elements[1] == item[1]) or (matching.elements[0] == item[1] and matching.elements[1] == item[0]):
+                matching_found = True
+        #if not matching_found and matching.elements[0] != matching.elements[1]:
+        if not matching_found:
+            tmp = list(matching.elements)
+            labels = matching.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
+            if labels is not None:
+                tmp = tmp + labels
+            else:
+                labels = comparison.get_properties_named("{http://www.w3.org/2000/01/rdf-schema#}label")
+                if labels is not None:
+                    tmp = tmp + labels
+            #print labels
+            #print tmp
+            different[1].append(tmp)
+    print "same" + "(" + str(len(same)) + ")" + ": " + str(same)
+    print "different" + "(" + str(len(different[0]) + len(different[1])) + ")" + ": " + str(different)
     return [same, different]
