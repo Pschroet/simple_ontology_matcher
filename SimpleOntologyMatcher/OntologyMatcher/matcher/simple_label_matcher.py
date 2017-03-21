0fc4503d83c839d0d2ce9e1cfead41d55571db33
Pschroet <pschroet@inf.fu-berlin.de>
2017-03-21 21:13:30 +0100

    added option exact_match to simple label matcher: only labels that are exactly the same will be added to possible connections, not where parts of them are the same (default is true)

--- SimpleOntologyMatcher/OntologyMatcher/matcher/simple_label_matcher.py
+++ SimpleOntologyMatcher/OntologyMatcher/matcher/simple_label_matcher.py
@@ -30,16 +30,40 @@ def match_two_ontologies(results, onto, onto1, exact_match = True):
                             if label1 != []:
                                 for item1 in label1:
                                     already_matched = False
-                                    match_result = re.match("^" + item.get_text().replace("_", " ") + "$", item1.get_text().replace("_", " "), re.IGNORECASE)
+                                    match_result = re.match("^" + item.get_text().replace("_", " ").lower() + "$", item1.get_text().replace("_", " ").lower(), re.IGNORECASE)
                                     if not already_matched and match_result and not re.match(i.name, j.name, re.IGNORECASE):
                                         #util.write2File("matching.txt", "Nodes " + i.name + " (" + label.get_text() + ")" + " and " + j.name + " (" + label1.get_text() + ")" + " have the same label\n", "a")
                                         connections["matches"].append([i.name, item.get_text(), j.name, item1.get_text(), " have the same label\n", "owl:sameAs"])
                                         already_matched = True
                                     ##version 2: added
-                                    #if the labels don't match already, check if one word of the labels is inside the other
-                                    elif not exact_match and not already_matched and ((re.match(item.get_text().replace("_", " "), item1.get_text().replace("_", " "), re.IGNORECASE) and len(item.get_text()) > 3) or (re.match(item1.get_text().replace("_", " "), item.get_text().replace("_", " "), re.IGNORECASE) and len(item1.get_text()) > 3)) and not re.match(i.name, j.name, re.IGNORECASE):
-                                        connections["matches"].append([i.name, item.get_text(), j.name, item1.get_text(), " share part of the label\n"])
-                                        already_matched = True
+                                    #if the labels don't match already, check if all words of the labels is inside the other
+                                    elif not exact_match and not already_matched and len(item.get_text()) > 3 and len(item1.get_text()) > 3 and not re.match(i.name, j.name, re.IGNORECASE):
+                                        #check the first of the two labels
+                                        item_parts = item.get_text().replace("_", " ").lower().split()
+                                        all_in_other = True
+                                        if len(item_parts) > 1:
+                                            for part in item_parts:
+                                                if not part in item1.get_text():
+                                                    all_in_other = False
+                                                    break
+                                        elif not item.get_text().lower() in item1.get_text().lower():
+                                            all_in_other = False
+                                        if all_in_other:
+                                            connections["matches"].append([i.name, item.get_text(), j.name, item1.get_text(), " share part of the label\n"])
+                                            already_matched = True
+                                        #check the other label
+                                        item1_parts = item.get_text().replace("_", " ").lower().split()
+                                        all_in_other = True
+                                        if len(item1_parts) > 1:
+                                            for part in item1_parts:
+                                                if not part in item.get_text():
+                                                    all_in_other = False
+                                                    break
+                                        elif not item1.get_text().lower() in item.get_text().lower():
+                                            all_in_other = False
+                                        if all_in_other:
+                                            connections["matches"].append([i.name, item.get_text(), j.name, item1.get_text(), " share part of the label\n"])
+                                            already_matched = True
                     #comment = i.get_child("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}comment")
                     #if comment != None:
                     #    match_result = re.match(".*" + comment.get_text() + ".*", j.name, re.IGNORECASE)
